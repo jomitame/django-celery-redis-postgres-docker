e15c74d82f14a9c00f30a2a19b6c28ba049a3811
jomitame <jomitame@gmail.com>
2022-08-09 19:36:53 -0500

    adding and configuring celery beat

--- app/app/celery.py
+++ app/app/celery.py
@@ -1,7 +1,18 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 app = Celery('app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'schedule_call_prin_after_time':{
+        'task': 'core.tasks.call_prin_after_time',
+        'args': ('from schedule',),
+        'schedule': crontab(minute='*/1')
+    },
+}
+
+app.conf.timezone = 'UTC'
